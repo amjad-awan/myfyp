17856bd414cec947b5138bc5549fd956f1d49de3
Nazim Raza <nazim.raza@aquila360.com>
2022-06-05 21:02:48 +0500

    app code fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-import CustomerLogin from './CustomerLogin';
-import Customerregistor from './Customer-registor';
-import WorkShopLogIn from "./WorkShopLogIn"
-import WorkShopRegistor from "./WorkShopRegistor"
-import MainShops from './MainShops';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import CustomerLogin from "./CustomerLogin";
+import Customerregistor from "./Customer-registor";
+import "react-toastify/dist/ReactToastify.css";
+import WorkShopLogIn from "./WorkShopLogIn";
+import WorkShopRegistor from "./WorkShopRegistor";
+import MainShops from "./MainShops";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
+import spinnerSvc from "./services/spinnerSvc";
+import LoaderSpinner from "./services/modal";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const loggedIn = localStorage.getItem("loggedIn");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(loggedIn);
+  useEffect(() => {
+    const subscription = spinnerSvc.requestInProgress.subscribe((isLoading) =>
+      setIsLoading(isLoading)
+    );
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<CustomerLogin/>} />
-          <Route path="/Customerregistor" element={<Customerregistor/>} />
-          <Route path="/WorkShopLogIn" element={<WorkShopLogIn/>} />
-          <Route path="/WorkShopRegistor" element={<WorkShopRegistor/>} />
-          <Route path="/MianShops" element={<MainShops/>} />
+    <>
+      <ToastContainer hideProgressBar={true} />
+      {isLoading && <LoaderSpinner />}
+      <div className="App">
+        <Router>
+          <Routes>
+            {!isLoggedIn ? (
+              <>
+                <Route path="*" element={<Navigate to="/" />} />
+                <Route
+                  path="/"
+                  element={<CustomerLogin onLogin={() => setLoggedIn(true)} />}
+                />
+                <Route
+                  path="/CustomerRegister"
+                  element={<Customerregistor />}
+                />
+                <Route
+                  path="/WorkShopLogIn"
+                  element={<WorkShopLogIn onLogin={() => setLoggedIn(true)} />}
+                />
+                <Route
+                  path="/WorkShopRegister"
+                  element={<WorkShopRegistor />}
+                />
+              </>
+            ) : (
+              <>
+                <Route path="/" element={<Navigate to="/MainShops" />} />
+                <Route path="*" element={<Navigate to="/MainShops" />} />
+                <Route
+                  path="/MainShops"
+                  element={<MainShops onLogout={() => setLoggedIn(false)} />}
+                />
+              </>
+            )}
           </Routes>
-          </Router>
-    </div>
+        </Router>
+      </div>
+    </>
   );
 }
 
